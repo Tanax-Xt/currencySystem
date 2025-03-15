6a6c843c06edaf543552c11c126734d713b8a5c5
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2025-03-15 18:28:15 +0300

    fix: fix validation

--- src/main/java/ru/tanaxxt/currencysystem/requests/CurrencyRequest.java
+++ src/main/java/ru/tanaxxt/currencysystem/requests/CurrencyRequest.java
@@ -2,20 +2,17 @@ package ru.tanaxxt.currencysystem.requests;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 
 @Getter
-@NoArgsConstructor
 public class CurrencyRequest {
     @NotBlank(message = "Name is required")
     private String name;
+
     @NotBlank(message = "Base currency is required")
     private String baseCurrency = "RUB";
+
     @NotBlank(message = "Price change range is required")
     private String priceChangeRange;
+
     private String description;
-
 }
-
-
