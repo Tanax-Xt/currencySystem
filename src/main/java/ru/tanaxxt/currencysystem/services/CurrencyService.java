e9d02cde92c78d2942031eed0ab3b0d593fa7a04
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2025-03-27 11:10:44 +0300

    feat: add transactions

--- src/main/java/ru/tanaxxt/currencysystem/services/CurrencyService.java
+++ src/main/java/ru/tanaxxt/currencysystem/services/CurrencyService.java
@@ -1,11 +1,10 @@
 package ru.tanaxxt.currencysystem.services;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.tanaxxt.currencysystem.entities.Currency;
 import ru.tanaxxt.currencysystem.repositories.CurrencyRepository;
-import ru.tanaxxt.currencysystem.requests.CurrencyRequest;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,19 +15,22 @@ public class CurrencyService {
 
     private final CurrencyRepository currencyRepository;
 
+    @Transactional(readOnly = true)
     public List<Currency> getAllCurrencies() {
         return currencyRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Currency getCurrencyById(UUID id) {
         return currencyRepository.findById(id).orElse(null);
     }
 
-
+    @Transactional
     public Currency addCurrency(Currency currency) {
         return currencyRepository.save(currency);
     }
 
+    @Transactional
     public Currency updateCurrency(UUID id, Currency currency) {
         Currency currencyToUpdate = currencyRepository.findById(id).orElse(null);
         if (currencyToUpdate == null) {
@@ -41,6 +43,7 @@ public class CurrencyService {
         return currencyRepository.save(currencyToUpdate);
     }
 
+    @Transactional
     public void deleteCurrency(UUID id) {
         currencyRepository.deleteById(id);
     }
