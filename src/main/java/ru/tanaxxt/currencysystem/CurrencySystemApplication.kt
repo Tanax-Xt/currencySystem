5c6f91832beef8d68b52e2aaa9fc6c7addc0e617
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2025-04-23 09:47:15 +0300

    fix: add annotation to scheduling

--- src/main/java/ru/tanaxxt/currencysystem/CurrencySystemApplication.kt
+++ src/main/java/ru/tanaxxt/currencysystem/CurrencySystemApplication.kt
@@ -2,9 +2,11 @@ package ru.tanaxxt.currencysystem
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @SpringBootApplication
+@EnableScheduling
 class CurrencySystemApplication
 
 fun main(args: Array<String>) {
