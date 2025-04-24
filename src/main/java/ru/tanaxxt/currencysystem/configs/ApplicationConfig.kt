cfd03e77c6c2e96ad14a5e6519d6c4bb11b85251
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2025-04-24 11:58:55 +0300

    refactor: rewrite configs to kotlin

--- src/main/java/ru/tanaxxt/currencysystem/configs/ApplicationConfig.kt
+++ src/main/java/ru/tanaxxt/currencysystem/configs/ApplicationConfig.kt
@@ -1,23 +1,17 @@
-package ru.tanaxxt.currencysystem.configs;
+package ru.tanaxxt.currencysystem.configs
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
 
 @Configuration
-@EnableConfigurationProperties(CurrencyConfig.class)
-public class ApplicationConfig {
+@EnableConfigurationProperties(CurrencyConfig::class)
+class ApplicationConfig {
+    @Bean
+    fun getObjectmapper() = ObjectMapper()
 
     @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public ObjectMapper getObjectmapper() {
-        return new ObjectMapper();
-    }
-
+    fun getRestTemplate() = RestTemplate()
 }
--- src/main/java/ru/tanaxxt/currencysystem/configs/CurrencyConfig.kt
+++ src/main/java/ru/tanaxxt/currencysystem/configs/CurrencyConfig.kt
@@ -1,12 +1,6 @@
-package ru.tanaxxt.currencysystem.configs;
+package ru.tanaxxt.currencysystem.configs
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties
 
-@Getter
-@AllArgsConstructor
 @ConfigurationProperties(prefix = "currency-tracker")
-public class CurrencyConfig {
-    private String cbApiUrl;
-}
+data class CurrencyConfig(val cbApiUrl: String)
